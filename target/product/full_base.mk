75eef1bc78901acda51e6d7a8f06eba0b30ae369
sdroyds <mike3582.mw@gmail.com>
2014-02-16 10:28:41 -0500

    build TwistedKat

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -24,16 +24,16 @@ PRODUCT_PACKAGES := \
     VideoEditor \
     WAPPushManager
 
-PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
-    VisualizationWallpapers \
-    PhotoTable
+#PRODUCT_PACKAGES += \
+#    Galaxy4 \
+#    HoloSpiralWallpaper \
+#    LiveWallpapers \
+#    LiveWallpapersPicker \
+#    MagicSmokeWallpapers \
+#    NoiseField \
+#    PhaseBeam \
+#    VisualizationWallpapers \
+#    PhotoTable
 
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
